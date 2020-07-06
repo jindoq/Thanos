c8a5243df6a17dc790416ec7f993902a9e621f14
jindoq <tranduytu93@gmail.com>
2020-07-06 13:44:22 +0700

    fix ads

--- Thanos/Thanos/Modules/ThanosCtr.swift
+++ Thanos/Thanos/Modules/ThanosCtr.swift
@@ -15,53 +15,22 @@ open class ThanosCtr: UIViewController {
         didSet {
             if isEnableAdmod {
                 bannerId = ""
-                interstitalId = ""
             } else {
                 // TEST
                 bannerId = "ca-app-pub-3940256099942544/2934735716"
-                interstitalId = "ca-app-pub-3940256099942544/4411468910"
             }
         }
     }
 
     private var bannerId = ""
-    private var interstitalId = ""
-
     var bannerView: GADBannerView!
-    var interstitial: GADInterstitial!
 
     override open func viewDidLoad() {
         super.viewDidLoad()
         isEnableAdmod = false
         
-        if !IAPProducts.store.isProductPurchased(IAPProducts.removeAds) {
-            interstitial = createAndLoadInterstitial()
-        }
-        
         if let views = navigationController?.viewControllers, views.count >= 2 {
             setupAds()
-        } else {
-            setupSetting()
-        }
-    }
-
-    public func showAdmodInterstital() {
-        guard !IAPProducts.store.isProductPurchased(IAPProducts.removeAds) else {
-            return
-        }
-        
-        if interstitial.isReady {
-            let requestAd = UserDefaults.standard.integer(forKey: "Request_Ad")
-            if requestAd == 0 {
-                interstitial.present(fromRootViewController: self)
-                UserDefaults.standard.set(requestAd + 1, forKey: "Request_Ad")
-            } else if requestAd >= 8 {
-                UserDefaults.standard.set(0, forKey: "Request_Ad")
-            } else {
-                UserDefaults.standard.set(requestAd + 1, forKey: "Request_Ad")
-            }
-        } else {
-            print("Ad wasn't ready")
         }
     }
 
@@ -77,14 +46,6 @@ open class ThanosCtr: UIViewController {
         bannerView.load(request)
         bannerView.delegate = self
         addBannerViewToView(bannerView)
-    }
-
-    private func createAndLoadInterstitial() -> GADInterstitial {
-        interstitial = GADInterstitial(adUnitID: interstitalId)
-        interstitial.delegate = self
-        let request = GADRequest()
-        interstitial.load(request)
-        return interstitial
     }
 
     private func addBannerViewToView(_ bannerView: GADBannerView) {
@@ -114,8 +75,11 @@ open class ThanosCtr: UIViewController {
     }
 
     @objc open func showAds() {
-        showAdmodInterstital()
-        navigationController?.popViewController(animated: true)
+        AdsManager.ads.showAdmodInterstital(vc: self) {
+            DispatchQueue.main.async {
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
     }
 
     open func setupSetting() {
@@ -132,6 +96,135 @@ open class ThanosCtr: UIViewController {
     @objc open func settingAction() {
         let vc = SettingCtr()
         push(vc)
+    }
+}
+
+extension ThanosCtr {
+    open func present(_ vc: UIViewController) {
+        vc.modalPresentationStyle = .fullScreen
+        AdsManager.ads.showAdmodInterstital(vc: self) {
+            DispatchQueue.main.async {
+                self.present(vc, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    open func push(_ vc: UIViewController) {
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .done, target: nil, action: nil)
+        AdsManager.ads.showAdmodInterstital(vc: self) {
+            DispatchQueue.main.async {
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+        }
+    }
+}
+
+public struct AdsManager {
+    public static let ads = AdsInterstitial()
+}
+
+public class AdsInterstitial: NSObject, GADInterstitialDelegate {
+    private var intervalTime: TimeInterval = 60
+    private var lastTime: TimeInterval?
+
+    private var callback: (() -> Void)?
+    private var interstitalId = ""
+    private var interstitial: GADInterstitial!
+    private var showing = true
+    
+    public func start(enable: Bool) {
+        if enable {
+            interstitalId = ""
+        } else {
+            // TEST
+            interstitalId = "ca-app-pub-3940256099942544/4411468910"
+        }
+        
+        interstitial = createAndLoadInterstitial()
+    }
+    
+    public func showAdmodInterstital(vc: UIViewController, completed: @escaping () -> Void) {
+        guard !IAPProducts.store.isProductPurchased(IAPProducts.removeAds) else {
+            completed()
+            return
+        }
+        
+        guard showing else {
+            completed()
+            return
+        }
+        
+        if lastTime == nil {
+            if interstitial.isReady {
+                lastTime = Date().timeIntervalSince1970
+                interstitial.present(fromRootViewController: vc)
+                callback = completed
+            } else {
+                completed()
+            }
+            return
+        }
+        
+        guard let time = lastTime else {
+            completed()
+            return
+        }
+        
+        guard time + intervalTime <= Date().timeIntervalSince1970 else {
+            completed()
+            return
+        }
+    
+        lastTime = Date().timeIntervalSince1970
+        
+        if interstitial.isReady {
+            interstitial.present(fromRootViewController: vc)
+            callback = completed
+        } else {
+            completed()
+        }
+    }
+    
+    public func createAndLoadInterstitial() -> GADInterstitial {
+        interstitial = GADInterstitial(adUnitID: interstitalId)
+        interstitial.delegate = self
+        let request = GADRequest()
+        interstitial.load(request)
+        return interstitial
+    }
+    
+    /// Tells the delegate an ad request succeeded.
+    public func interstitialDidReceiveAd(_ ad: GADInterstitial) {
+        print("interstitialDidReceiveAd")
+    }
+
+    /// Tells the delegate an ad request failed.
+    public func interstitial(_ ad: GADInterstitial, didFailToReceiveAdWithError error: GADRequestError) {
+        print("interstitial:didFailToReceiveAdWithError: \(error.localizedDescription)")
+    }
+
+    /// Tells the delegate that an interstitial will be presented.
+    public func interstitialWillPresentScreen(_ ad: GADInterstitial) {
+        print("interstitialWillPresentScreen")
+    }
+
+    /// Tells the delegate the interstitial is to be animated off the screen.
+    public func interstitialWillDismissScreen(_ ad: GADInterstitial) {
+        print("interstitialWillDismissScreen")
+    }
+
+    /// Tells the delegate the interstitial had been animated off the screen.
+    public func interstitialDidDismissScreen(_ ad: GADInterstitial) {
+        callback?()
+        interstitial = createAndLoadInterstitial()
+        print("interstitialDidDismissScreen")
+    }
+
+    /// Tells the delegate that a user click will open another app
+    /// (such as the App Store), backgrounding the current app.
+    public func interstitialWillLeaveApplication(_ ad: GADInterstitial) {
+        showing = false
+        print("interstitialWillLeaveApplication")
     }
 }
 
@@ -171,59 +264,5 @@ extension ThanosCtr: GADBannerViewDelegate{
     /// the App Store), backgrounding the current app.
     public func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
         print("adViewWillLeaveApplication")
-    }
-}
-
-extension ThanosCtr: GADInterstitialDelegate {
-
-    /// Tells the delegate an ad request succeeded.
-    public func interstitialDidReceiveAd(_ ad: GADInterstitial) {
-        print("interstitialDidReceiveAd")
-    }
-
-    /// Tells the delegate an ad request failed.
-    public func interstitial(_ ad: GADInterstitial, didFailToReceiveAdWithError error: GADRequestError) {
-        print("interstitial:didFailToReceiveAdWithError: \(error.localizedDescription)")
-    }
-
-    /// Tells the delegate that an interstitial will be presented.
-    public func interstitialWillPresentScreen(_ ad: GADInterstitial) {
-        print("interstitialWillPresentScreen")
-    }
-
-    /// Tells the delegate the interstitial is to be animated off the screen.
-    public func interstitialWillDismissScreen(_ ad: GADInterstitial) {
-        print("interstitialWillDismissScreen")
-    }
-
-    /// Tells the delegate the interstitial had been animated off the screen.
-    public func interstitialDidDismissScreen(_ ad: GADInterstitial) {
-        interstitial = createAndLoadInterstitial()
-        print("interstitialDidDismissScreen")
-    }
-
-    /// Tells the delegate that a user click will open another app
-    /// (such as the App Store), backgrounding the current app.
-    public func interstitialWillLeaveApplication(_ ad: GADInterstitial) {
-        print("interstitialWillLeaveApplication")
-    }
-}
-
-
-extension ThanosCtr {
-    open func present(_ vc: UIViewController) {
-        showAdmodInterstital()
-        vc.modalPresentationStyle = .fullScreen
-        DispatchQueue.main.async {
-            self.present(vc, animated: true, completion: nil)
-        }
-    }
-    
-    open func push(_ vc: UIViewController) {
-        showAdmodInterstital()
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .done, target: nil, action: nil)
-        DispatchQueue.main.async {
-            self.navigationController?.pushViewController(vc, animated: true)
-        }
     }
 }
